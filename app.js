df999511af01d9a4a4b22ec6ecbbede3cbd8762c
Paul Henderson <ptmhende@uvic.ca>
2022-01-13 15:03:26 -0800

    2021 over, moving on to 2022

--- app.js
+++ app.js
@@ -14,34 +14,66 @@ var click = function() {
     
   }
 
-function generate(event) { 
+function generate(event, index, pos, sz, chars) { 
     if (counter == 0) {
         draw = SVG().addTo('#create').size(1000,1000)
         //draw = SVG().addTo('#canvas').size('100%','100%')
         counter += 1;
     }
     counter += 1;
-    var x = event.keyCode;
-    if (x == 13) {
-        var rect = draw.rect(sizeVar, sizeVar).attr({ fill: 'teal' }).x(moveX).y(moveY)
+    sizeVar = sz
+    var kc = event.keyCode;
+    if (kc == 13) {
+        console.log("POS ON ENTER:", pos);
+        if(chars) {
+            var rect = draw.rect(sizeVar, sizeVar).attr({ fill: 'teal' }).x(moveX).y((pos-(sz/2)))
+            if (counter %3 == 0) {
+                rect.fill({ color: 'red' })
+            }
+        } else {
+            var rect = draw.rect(sizeVar, sizeVar).attr({ fill: 'transparent' }).x(moveX).y((pos))
+        }
         rect.on('click', click)
-        if (counter%2 == 1) {
+        /* if (counter%2 == 1) {
             moveX += sizeVar;
         } else {
             moveX -= sizeVar;
+        } */
+        //moveY += sizeVar;
+        index += 1
+        while(draw.get(index)) {
+            draw.get(index).dy(sizeVar)
+            index += 1
         }
-        moveY += sizeVar;
     }
 
 }
 
-function rmv_svg() { 
-    counter += 1
-    draw.last().remove()
-    if (counter%2 == 1) {
+function rmv_svg(index, pos, sz, curr, next) { 
+    //counter += 1
+    sizeVar = sz
+    console.log("PASSED POS:", pos);
+    tempIndex = index
+    for(let i=0; i<(curr-next); i++) {
+        console.log("INDEX", index);
+        console.log("TEMP", tempIndex);
+        draw.get(tempIndex).remove()
+        if (tempIndex > 0) {
+            tempIndex -= 1
+        }
+        //pos -= sizeVar
+    }
+    
+    /*if (counter%2 == 1) {
         moveX += sizeVar;
     } else {
         moveX -= sizeVar;
+    } */
+    //moveY -= sizeVar 
+    while(draw.get(tempIndex)) {
+        for(let i=0; i<curr-next; i++) {
+            draw.get(tempIndex).dy(-sizeVar)
+        }
+        tempIndex += 1
     }
-    moveY -= sizeVar
 }
