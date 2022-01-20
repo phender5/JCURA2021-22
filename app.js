076198396abefbf09fcdde1a5320ba36fe88de97
Paul Henderson <ptmhende@uvic.ca>
2022-01-20 00:44:09 -0800

    Debugged a lot of the batch deleting and insertions, much more to do

--- app.js
+++ app.js
@@ -16,64 +16,59 @@ var click = function() {
 
 function generate(event, index, pos, sz, chars) { 
     if (counter == 0) {
-        draw = SVG().addTo('#create').size(1000,1000)
-        //draw = SVG().addTo('#canvas').size('100%','100%')
-        counter += 1;
+        //draw = SVG().addTo('#create').size(1000,1000)
+        draw = SVG().addTo('#create').size('100%','100%')
+        counter += 2;
     }
-    counter += 1;
     sizeVar = sz
     var kc = event.keyCode;
     if (kc == 13) {
         console.log("POS ON ENTER:", pos);
         if(chars) {
-            var rect = draw.rect(sizeVar, sizeVar).attr({ fill: 'teal' }).x(moveX).y((pos-(sz/2)))
+            var rect = draw.rect(sizeVar, sizeVar).attr({ fill: 'teal' }).x(moveX).y((pos-(sizeVar/2)))
+            counter += 1;
             if (counter %3 == 0) {
                 rect.fill({ color: 'red' })
             }
         } else {
-            var rect = draw.rect(sizeVar, sizeVar).attr({ fill: 'transparent' }).x(moveX).y((pos))
+            var rect = draw.rect(sizeVar, sizeVar).attr({ fill: 'transparent' }).x(moveX).y((pos-(sizeVar/2)))
         }
-        rect.on('click', click)
-        /* if (counter%2 == 1) {
-            moveX += sizeVar;
-        } else {
-            moveX -= sizeVar;
-        } */
-        //moveY += sizeVar;
-        index += 1
+
+        //rect.on('click', click)
+        //index += 1
         while(draw.get(index)) {
-            draw.get(index).dy(sizeVar)
+            var nextOnes = draw.get(index)
+            console.log("NEXT ONES POS: ", nextOnes.y());
+            nextOnes.dy(sizeVar)
+            nextOnes.fill({ color: "pink"})
             index += 1
         }
     }
 
 }
 
-function rmv_svg(index, pos, sz, curr, next) { 
-    //counter += 1
+function rmv_svg(lc, sz, chg, curr) { 
     sizeVar = sz
-    console.log("PASSED POS:", pos);
-    tempIndex = index
-    for(let i=0; i<(curr-next); i++) {
-        console.log("INDEX", index);
-        console.log("TEMP", tempIndex);
-        draw.get(tempIndex).remove()
-        if (tempIndex > 0) {
-            tempIndex -= 1
+    console.log("PASSED MAX: ", lc);
+    tempIndex = curr
+    for(let i=0; i<(chg); i++) {
+        console.log("CHANGE", chg);
+        if (draw.get(tempIndex)) {
+            draw.get(tempIndex).remove()
+            tempIndex += 1
         }
         //pos -= sizeVar
     }
-    
-    /*if (counter%2 == 1) {
-        moveX += sizeVar;
-    } else {
-        moveX -= sizeVar;
-    } */
-    //moveY -= sizeVar 
-    while(draw.get(tempIndex)) {
-        for(let i=0; i<curr-next; i++) {
-            draw.get(tempIndex).dy(-sizeVar)
-        }
-        tempIndex += 1
-    }
+    while(draw.get(curr)) {
+        
+        console.log("XCHANGE", chg);
+        draw.get(curr).dy((-sizeVar*(chg)))
+        curr += 1
+    } 
+    // while(draw.get(tempIndex)) {
+        
+    //     console.log("XCHANGE", chg);
+    //     draw.get(tempIndex).dy((-sizeVar*(chg)))
+    //     tempIndex += 1
+    // } 
 }
