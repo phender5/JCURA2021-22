06ab8b2430ba3abc944d9ade8a16ec65a6ab7e20
Paul Henderson <ptmhende@uvic.ca>
2022-01-27 13:03:34 -0800

    Scrolling functionality SOLVEDgit add .

--- app.js
+++ app.js
@@ -13,8 +13,14 @@ var click = function() {
     }
     
   }
+  
+  const setBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+   return "#" + randomColor;
+  }
 
-function generate(event, index, pos, sz, chars) { 
+// change svg to something more distinct
+function generate(event, posIn, sz, chars) { 
     if (counter == 0) {
         //draw = SVG().addTo('#create').size(1000,1000)
         draw = SVG().addTo('#create').size('100%','100%')
@@ -22,53 +28,62 @@ function generate(event, index, pos, sz, chars) {
     }
     sizeVar = sz
     var kc = event.keyCode;
+    pos = posIn-sizeVar
     if (kc == 13) {
         console.log("POS ON ENTER:", pos);
         if(chars) {
-            var rect = draw.rect(sizeVar, sizeVar).attr({ fill: 'teal' }).x(moveX).y((pos-(sizeVar/2)))
+            var rect = draw.rect(sizeVar, sizeVar).attr({ fill: setBg() }).x(moveX).y((pos-(sizeVar/2)))
             counter += 1;
-            if (counter %3 == 0) {
-                rect.fill({ color: 'red' })
-            }
         } else {
             var rect = draw.rect(sizeVar, sizeVar).attr({ fill: 'transparent' }).x(moveX).y((pos-(sizeVar/2)))
         }
 
         //rect.on('click', click)
         //index += 1
-        while(draw.get(index)) {
-            var nextOnes = draw.get(index)
-            console.log("NEXT ONES POS: ", nextOnes.y());
-            nextOnes.dy(sizeVar)
-            nextOnes.fill({ color: "pink"})
-            index += 1
-        }
+        draw.each(function(i, children) {
+            if (this.cy()>pos) {
+                this.dy(sizeVar);
+            } 
+        });
     }
 
 }
 
-function rmv_svg(lc, sz, chg, curr) { 
+function rmv_svg(pos, sz, chamt, curr) { 
     sizeVar = sz
-    console.log("PASSED MAX: ", lc);
-    tempIndex = curr
-    for(let i=0; i<(chg); i++) {
-        console.log("CHANGE", chg);
-        if (draw.get(tempIndex)) {
-            draw.get(tempIndex).remove()
-            tempIndex += 1
-        }
-        //pos -= sizeVar
-    }
-    while(draw.get(curr)) {
-        
-        console.log("XCHANGE", chg);
-        draw.get(curr).dy((-sizeVar*(chg)))
-        curr += 1
-    } 
-    // while(draw.get(tempIndex)) {
-        
-    //     console.log("XCHANGE", chg);
-    //     draw.get(tempIndex).dy((-sizeVar*(chg)))
-    //     tempIndex += 1
-    // } 
+    countDown = chamt
+    tempIndex = curr + sizeVar
+    draw.each(function(i, children) {
+        //console.log("CURR in loop:", curr);
+        if ((this.cy()==tempIndex || curr==12) && countDown>0) {
+            this.remove();
+            tempIndex += sizeVar
+            countDown -= 1
+        } 
+    });
+    console.log("SHOW ME RMV POS: ", pos);
+    // for(let i=0; i<(chamt); i++) {
+    //     console.log("CHANGE", chamt);
+    //     if (draw.get(tempIndex)) {
+    //         draw.get(tempIndex).remove()
+    //         tempIndex += 1
+    //     }
+  
+    // }
+    draw.each(function(i, children) {
+        if (this.cy()>(pos+sizeVar)) {
+            this.dy(-sizeVar*(chamt));
+        } 
+    });
+} 
+    
+    //scroll info top: 
+    // amount = current - prev
+    // svg.js each()
+    // this.dy(amount)
+function moveOnScroll(currPos, prevPos) {
+    moveSize = prevPos - currPos;
+    draw.each(function(i, children) {
+        this.dy(moveSize);
+    });
 }
